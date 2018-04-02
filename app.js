fa046c80e60d7211f2fb4bb238f78fa1474428a1
Oscar <oscar@swuser-129-8-201-155.swlan.csufresno.edu>
2018-04-02 15:47:01 -0700

    adding in code for the getting data portion of section 1

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 var express = require('express');
+mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/bookAPI');
 var app = express();
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+var Book = require('./models/bookModel');
+bookRouter.route('/Books')
+.get(function(req,res){
+
+    var query = {};
+    if(req.query.genre){
+        query.genre = req.query.genre;
+    }
+    Book.find(query, function(err,books){
+        if(err)
+res.status(500).send(err)
+        else
+        res.json(books);
+    })
+    res.json(responseJson);
+});
+bookRouter.route('/Books/:bookId')
+.get(function(req,res){
+    Book.findById(rq.params.bookId, function(err,book){
+        if(err)
+res.status(500).send(err)
+        else
+        res.json(book);
+    })
+    res.json(responseJson);
+});
+app.use('/api', bookRouter);
+
 
 app.get('/', function(req, res){
     res.send('welcome to my API!')
@@ -9,3 +42,4 @@ app.get('/', function(req, res){
 app.listen(port, function(){
     console.log('Gulp is running my app on PORT: '+ port);
 });
+
