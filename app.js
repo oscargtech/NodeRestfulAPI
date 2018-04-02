0c6ca9cd333988e456f06fd9a0dfbc447fc398cc
Oscar <oscar@swuser-129-8-201-155.swlan.csufresno.edu>
2018-04-02 14:23:30 -0700

    adding contents for what is rest? section of assignment 5

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send('welcome to my API!')
+});
+
+app.listen(port, function(){
+    console.log('Gulp is running my app on PORT: '+ port);
+});
