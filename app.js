3d1636495c22097b1e49f0c0ab6c1228b771cb5c
Oscar <oscar@swuser-129-8-200-105.swlan.csufresno.edu>
2018-05-06 20:50:32 -0700

    Adding Posting Data section of assignemtn

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-var express = require('express');
-mongoose = require('mongoose');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
 var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ estended: true }));
+app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-var Book = require('./models/bookModel');
-bookRouter.route('/Books')
-.get(function(req,res){
-
-    var query = {};
-    if(req.query.genre){
-        query.genre = req.query.genre;
-    }
-    Book.find(query, function(err,books){
-        if(err)
-res.status(500).send(err)
-        else
-        res.json(books);
-    })
-    res.json(responseJson);
-});
-bookRouter.route('/Books/:bookId')
-.get(function(req,res){
-    Book.findById(rq.params.bookId, function(err,book){
-        if(err)
-res.status(500).send(err)
-        else
-        res.json(book);
-    })
-    res.json(responseJson);
-});
-app.use('/api', bookRouter);
+app.use('/api'/books, bookRouter);
+app.use('/api'/authors, bookRouter);
 
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
     res.send('welcome to my API!')
 });
 
-app.listen(port, function(){
-    console.log('Gulp is running my app on PORT: '+ port);
+app.listen(port, function () {
+    console.log('Gulp is running my app on PORT: ' + port);
 });
 
